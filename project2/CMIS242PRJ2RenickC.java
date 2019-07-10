3f974d103ff8b7ea9e41bfa0a7dabc6ccae88a25
Chase Renick <RWolf16@users.noreply.github.com>
2019-07-09 23:34:43 -0400

    0.0.1

---
+++ project2/CMIS242PRJ2RenickC.java
@@ -0,0 +1,22 @@
+/*
+ * File: CMIS242PRJ3RenickC.java
+ * Author: Chase Renick
+ * Date: July 10, 2019
+ * Purpose: This program is designed to do a calculation recursively and iteratively
+ * based on what a user types into the program.
+ */
+
+//Installing Necessary Packages
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.io.*;
+import java.awt.event.*;
+import java.awt.SystemColor;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
