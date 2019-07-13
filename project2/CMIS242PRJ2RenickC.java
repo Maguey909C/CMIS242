bed2d3fb81a42faf1cac50855fabdc0c7f37409e
Chase Renick <RWolf16@users.noreply.github.com>
2019-07-13 13:02:29 -0400

    0.0.4

--- project2/CMIS242PRJ2RenickC.java
+++ project2/CMIS242PRJ2RenickC.java
@@ -1,7 +1,7 @@
 /*
  * File: CMIS242PRJ3RenickC.java
  * Author: Chase Renick
- * Date: July 13, 2019
+ * Date: July 2019
  * Purpose: This program is designed to do a calculation recursively and iteratively
  * based on what a user types into the program.
  */
@@ -16,99 +16,145 @@ import java.util.ArrayList;
 import java.util.stream.Collectors; 
 
 //Creating Class
-public class CMIS242PRJ4RenickC {  
+public class CMIS242PRJ3RenickC {  
+    
 	public static Integer checkingAccount = 0;
 	public static Integer savingsAccount = 0;
 	
-	public static class GUI extends JFrame {
+	public static void WriteFile (ArrayList<String> nTimes, ArrayList<String> results, ArrayList<String> efficiencies) {
+		String fileName = "efficiency.csv";
+		String fileName2 = "efficieny.txt";
+		try {
+			 // Assume default encoding.
+			 FileWriter fileWriter = new FileWriter(fileName);
+			 FileWriter fileWriter2 = new FileWriter(fileName2);
+	
+		     // Always wrap FileWriter in BufferedWriter.
+		     BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+		     BufferedWriter bufferedWriter2 = new BufferedWriter(fileWriter2);
+		
+		     // Note that write() does not automatically
+		     // append a newline character.
+		     
+		     String line1 = nTimes.stream().collect(Collectors.joining(","));
+		     String line3 = results.stream().collect(Collectors.joining(","));
+		     String line5 = efficiencies.stream().collect(Collectors.joining(","));
+		     
+		     //Writing to File 1 (csv)
+		     bufferedWriter.write(line1);
+		     bufferedWriter.newLine();
+		     bufferedWriter.write(line3);
+		     bufferedWriter.newLine();
+		     bufferedWriter.write(line5);
+		     
+		     //Writing to File 2 (txt)
+		     bufferedWriter2.write(line1);
+		     bufferedWriter2.newLine();
+		     bufferedWriter2.write(line3);
+		     bufferedWriter2.newLine();
+		     bufferedWriter2.write(line5);
+		     
+
+		     //Closing Files
+		     bufferedWriter.close();
+		     bufferedWriter2.close();
+		     }
+		catch(IOException ex) {
+			System.out.println("Error writing to file '"+ fileName + "'");
+		     // ex.printStackTrace();
+			}
+		}// end WriteFile
+	
+    public static class GUI extends JFrame {
         
-		JTextField textField,textField_1,textField_2; //The values that are used to manipulate the data of the array
-		JLabel entry1, entry2, entry3; //References to aid the user in the program
-		private static JRadioButton checking, saving; //JRadio buttons
-
-		private static JFrame frame; //Creates the Frame for display messages
+        JTextField textField,textField_1,textField_2; //The values that are used to manipulate the data of the array
+        JLabel entry1, entry2, entry3; //References to aid the user in the program
+        private static JRadioButton checking, saving; //JRadio buttons
+        
+        private static JFrame frame; //Creates the Frame for display messages
    
-		public GUI() {
+        public GUI() {
+            
+            // Making layout GridBagLayout ()     
+            setLayout(new GridBagLayout());
+            frame = new JFrame();
+            
+            //Instance of the GUI
+            GridBagConstraints window = new GridBagConstraints();
+            
+            //Sets up a 5x5 gird
+            window.insets = new Insets (5, 5, 5, 5);
+            
+            //Incorporate the Radio Buttons and bounds
+			checking = new JRadioButton("Checking"); 
+			window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 0;
+            window.gridy = 3;
+            window.gridwidth = 1;
+			checking.setSelected(true); //Having the JRadio button of "iterative" selected upon opening the application
+			add(checking, window);
+			
+			saving = new JRadioButton("Savings");
+			window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 1;
+            window.gridy = 3;
+            window.gridwidth = 1;
+			add(saving, window);
+			
+			// Group the radio buttons to toggle between two groups
+			ButtonGroup group = new ButtonGroup();
+			group.add(checking);
+			group.add(saving);
 
-		    // Making layout GridBagLayout ()     
-		    setLayout(new GridBagLayout());
-		    frame = new JFrame();
-
-		    //Instance of the GUI
-		    GridBagConstraints window = new GridBagConstraints();
-
-		    window.insets = new Insets (5, 5, 5, 5);
-
-		    //Incorporate the Radio Buttons and bounds
-		    checking = new JRadioButton("Checking");  //Setting the Jradio button iterative as final
-		    window.fill = GridBagConstraints.HORIZONTAL;
-		    window.gridx = 0;
-		    window.gridy = 3;
-		    window.gridwidth = 1;
-		    checking.setSelected(true); //Having the JRadio button of "iterative" selected upon opening the application
-		    add(checking, window);
-
-		    saving = new JRadioButton("Savings"); //Setting the JRadio button recursive
-		    window.fill = GridBagConstraints.HORIZONTAL;
-		    window.gridx = 1;
-		    window.gridy = 3;
-		    window.gridwidth = 1;
-				add(saving, window);
-
-		    // Group the radio buttons to toggle between two groups
-		    ButtonGroup group = new ButtonGroup();
-		    group.add(checking);
-		    group.add(saving);
-
-		    //JButton
-		    JButton withdraw = new JButton("Withdraw");
-		    window.fill = GridBagConstraints.HORIZONTAL;
-		    window.gridx = 0;
-		    window.gridy = 0;
-		    window.gridwidth = 1;
-		    add (withdraw, window);
-
-		    JButton deposit = new JButton("Deposit");
-		    window.fill = GridBagConstraints.HORIZONTAL;
-		    window.gridx = 1;
-		    window.gridy = 0;
-		    window.gridwidth = 1;
-		    add (deposit, window);
-
-		    JButton transfer = new JButton("Transfer to");
-		    window.fill = GridBagConstraints.HORIZONTAL;
-		    window.gridx = 0;
-		    window.gridy = 1;
-		    window.gridwidth = 1;
-		    add (transfer, window);
-	//            
-		    JButton balance = new JButton("Balance");
-		    window.fill = GridBagConstraints.HORIZONTAL;
-		    window.gridx = 1;
-		    window.gridy = 1;
-		    window.gridwidth = 1;
-		    add (balance, window);
-
-		    // Textfield 1
-		    textField = new JTextField(10);
-		    window.fill = GridBagConstraints.HORIZONTAL;
-		    window.gridx = 0;
-		    window.gridy = 4;
-		    window.gridwidth = 1;
-		    add (textField, window);
-
-
-		    //Make instances of events in the program
-		    Compute_Listener converter_listener = new Compute_Listener();
-		    //Each event attached to listener.
-		    deposit.addActionListener(converter_listener);
-		    withdraw.addActionListener(converter_listener);
-		    balance.addActionListener(converter_listener);
-		    transfer.addActionListener(converter_listener);
-		    } // end  GUI()    
+            //Various buttons for user to click on to later perform some action on the account
+			JButton withdraw = new JButton("Withdraw");
+            window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 0;
+            window.gridy = 0;
+            window.gridwidth = 1;
+            add (withdraw, window);
+            
+            JButton deposit = new JButton("Deposit");
+            window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 1;
+            window.gridy = 0;
+            window.gridwidth = 1;
+            add (deposit, window);
+            
+            JButton transfer = new JButton("Transfer to");
+            window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 0;
+            window.gridy = 1;
+            window.gridwidth = 1;
+            add (transfer, window);
+//            
+            JButton balance = new JButton("Balance");
+            window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 1;
+            window.gridy = 1;
+            window.gridwidth = 1;
+            add (balance, window);
+            
+            // Textfield 1
+            textField = new JTextField(10);
+            window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 0;
+            window.gridy = 4;
+            window.gridwidth = 1;
+            add (textField, window);
+            
+   
+            //Make instances of events in the program
+            Compute_Listener converter_listener = new Compute_Listener();
+            //Each event attached to listener.
+            deposit.addActionListener(converter_listener);
+            withdraw.addActionListener(converter_listener);
+            balance.addActionListener(converter_listener);
+            transfer.addActionListener(converter_listener);
+            } // end  GUI()    
 
       /**
-       *This class handles all the action listeners. Handles 
+       *This class handles all the action listeners. Takes care of the logic for each of the buttons when a user action clicks 
        */  
 		public class Compute_Listener implements ActionListener  {
 
@@ -120,10 +166,9 @@ public class CMIS242PRJ4RenickC {
 			            String operators = commands.getActionCommand(); // Finding particular command
 			            Boolean answer = checking.isSelected(); // Getting answer from whether 
 			            
-			            if(operators.equals("Deposit")){ //
-			            	Account.despositIt(userInput, answer);   
-			            	System.out.println("Entered Deposit");       
-			            } else if (operators.equals("Withdraw")) {
+			            if(operators.equals("Deposit")){ // Looks for whether user clicked on the deposit button and then performs necessary operations
+			            	Account.despositIt(userInput, answer);        
+			            } else if (operators.equals("Withdraw")) { //Looks for whether user clicked on the withdraw button and then performs operations
 			            	Account.withdrawIt(userInput, answer);
 			            } else if (operators.equals("Balance")) {
 			            	Account.showBalance(answer); 	
@@ -154,30 +199,28 @@ public class CMIS242PRJ4RenickC {
 			 }
 		} // end CheckValue
 		
-		//Function:
-		//Input:
-		//Output:
+		//Function: To show the current balance of the appropriate account 
+		//Input: Boolean of whether or not the account is checking
+		//Output: A pop-up window specifying the current balance in the appropriate account
 		public static void showBalance(Boolean answer) {
 			if (answer) {
 				JOptionPane.showMessageDialog(frame, "Checking Balance: " + checkingAccount.toString());
 			} else {
 			JOptionPane.showMessageDialog(frame, "Savings Balance: " + savingsAccount.toString());
 			}
-		}
+		} //End showBalance
 		
-		//Function: 
-	    //Input: 
-	    //Output: 
+		//Function: To deposit funds into the appropriate account
+	    //Input: The amount to be deposited, and whether or not the amount is going into checking
+	    //Output: The amount deposited into the account
 		static void despositIt(String userInput, Boolean answer) {
 			try {
             	if (checkValue(userInput)) {
             		int intInput = Integer.parseInt(userInput);
-            		if (answer){ //if the iterative button is selected, do the following		
-						checkingAccount += intInput; //call the computerRecursive method which will calculate fibonacci sequence iteratively
-						System.out.println(checkingAccount);
-					} else { //if the recursive button is selected do the following
-						savingsAccount += intInput;	
-						System.out.println(savingsAccount);
+            		if (answer){ //if it is checking, then do the following		
+						checkingAccount += intInput; //
+					} else { // do it to the savings 
+						savingsAccount += intInput;
 						};
 					} else {
 						JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
@@ -185,19 +228,19 @@ public class CMIS242PRJ4RenickC {
 				} catch (Exception ex) { //If the user types in something that is not valid then do the following
                     JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
                 }
-	    } // end static deposit it
+	    } // end static depositIt
 
-		//Function: 
-	    //Input: 
-	    //Output:
+		//Function: Checks whether appropriate funds based on the user can be withdrawn from the appropriate account
+	    //Input: The users specified amount, and whether or not it will be in checking or savings
+	    //Output: The amount withdrawn from the appropriate account
 		static void withdrawIt (String userInput, Boolean answer) {
 			try {
             	if (checkValue(userInput)) {
             		int intInput = Integer.parseInt(userInput);
             		if (answer){ //		
-						checkingAccount -= intInput; //
+						insufficientFunds.fundStatus(intInput, answer);
 					} else { //
-						savingsAccount -= intInput;		
+						insufficientFunds.fundStatus(intInput, answer);
 						};
 					} else {
 						JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
@@ -207,18 +250,20 @@ public class CMIS242PRJ4RenickC {
                 }
 		}// End withdrawIt
 		
-		//Function: 
-	    //Input: 
-	    //Output:
+		//Function: Function to transfer money from one account to another
+	    //Input: UserInput which will be parsed, and whether or not it is going into checking or savings
+	    //Output: Transfers an amount to a particular account 
 		static void transferTo (String userInput, Boolean answer) {
 			try {
             	if (checkValue(userInput)) {
             		int intInput = Integer.parseInt(userInput);
             		if (answer){ //		
-            			checkingAccount = checkingAccount - intInput;
+//            			checkingAccount = checkingAccount - intInput;
+            			insufficientFunds.fundStatus(intInput, answer);
 						savingsAccount += intInput;
 					} else { //
-						savingsAccount = savingsAccount - intInput;
+//						savingsAccount = savingsAccount - intInput;
+						insufficientFunds.fundStatus(intInput, answer);
 						checkingAccount += intInput;
 						};
 					} else {
@@ -230,8 +275,33 @@ public class CMIS242PRJ4RenickC {
 		}// End withdrawIt
 	} // public static class Account
 	
+	
+	//InsufficientFunds Class
 	public static class insufficientFunds {
 		
+		private static JFrame frame;
+		
+		//Function: Checks the status of the account
+		//Input: Integer version of user input, as well as whether or not to deduct from checking account or savings
+		//Output: Either a window saying appropriate account is insufficient, or a deduction from that account based on the intInput
+		static void fundStatus (Integer intInput, Boolean answer) {
+			
+			if (answer) {
+				int newBalance = checkingAccount -intInput; // Checks to see if checking account balance will be negative 
+				if (newBalance < 0){ //if it is negative then alert the user there are insufficient funds
+					JOptionPane.showMessageDialog(frame, "Insufficient Funds to withdraw from checking account!");
+				} else { //Otherwise deduct the amount
+					checkingAccount -= intInput;
+				}
+			} else {
+				int newBalance = savingsAccount -intInput; //Checks to see if savings account balance will be negative
+				if (newBalance < 0){ //if it is negative then alert the user there insufficient funds
+					JOptionPane.showMessageDialog(frame, "Insufficient Funds to withdraw from savings account!");
+				} else { //Otherwise deduct the amount 
+					savingsAccount -=  intInput;
+				}
+			}
+		}
 	}
 	
 	//Main Block
