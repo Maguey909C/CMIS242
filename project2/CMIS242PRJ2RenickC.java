622ee93e3d9a4f2a2b0d2abe5337dfaca8788489
Chase Renick <RWolf16@users.noreply.github.com>
2019-07-13 04:06:10 -0400

    0.0.3

--- project2/CMIS242PRJ2RenickC.java
+++ project2/CMIS242PRJ2RenickC.java
@@ -1,207 +1,294 @@
 /*
  * File: CMIS242PRJ3RenickC.java
  * Author: Chase Renick
- * Date: July 10, 2019
+ * Date: July 13, 2019
  * Purpose: This program is designed to do a calculation recursively and iteratively
  * based on what a user types into the program.
  */
 
-//Installing Necessary Packages
-import java.awt.EventQueue;
+//Importing Necessary Packages
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
-import java.awt.event.*;
 import java.awt.SystemColor;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
+import java.util.stream.Collectors; 
 
-
-public class CMIS242PRJ2RenickC {
-	
-	private static JFrame frame;
-	private static JTextField textField;
-	
+//Creating Class
+public class CMIS242PRJ4RenickC {  
+    
 	public static Integer checkingAccount = 0;
 	public static Integer savingsAccount = 0;
 	
-	public static void confirmAndExit() {
-		if (JOptionPane.showConfirmDialog(frame,  "You want to close the program?","Please confirm", JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION) {
-			System.exit(0);
-		}
-	}
+	public static void WriteFile (ArrayList<String> nTimes, ArrayList<String> results, ArrayList<String> efficiencies) {
+		String fileName = "efficiency.csv";
+		String fileName2 = "efficieny.txt";
+		try {
+			 // Assume default encoding.
+			 FileWriter fileWriter = new FileWriter(fileName);
+			 FileWriter fileWriter2 = new FileWriter(fileName2);
 	
-	public static void showBalance(String accountName, Integer balance) {
-		JOptionPane.showMessageDialog(frame, accountName+" Balance: " + balance.toString());
-	}
-	
-	public static class GUI {
+		     // Always wrap FileWriter in BufferedWriter.
+		     BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+		     BufferedWriter bufferedWriter2 = new BufferedWriter(fileWriter2);
 		
-	}
+		     // Note that write() does not automatically
+		     // append a newline character.
+		     
+		     String line1 = nTimes.stream().collect(Collectors.joining(","));
+		     String line3 = results.stream().collect(Collectors.joining(","));
+		     String line5 = efficiencies.stream().collect(Collectors.joining(","));
+		     
+		     //Writing to File 1 (csv)
+		     bufferedWriter.write(line1);
+		     bufferedWriter.newLine();
+		     bufferedWriter.write(line3);
+		     bufferedWriter.newLine();
+		     bufferedWriter.write(line5);
+		     
+		     //Writing to File 2 (txt)
+		     bufferedWriter2.write(line1);
+		     bufferedWriter2.newLine();
+		     bufferedWriter2.write(line3);
+		     bufferedWriter2.newLine();
+		     bufferedWriter2.write(line5);
+		     
+
+		     //Closing Files
+		     bufferedWriter.close();
+		     bufferedWriter2.close();
+		     }
+		catch(IOException ex) {
+			System.out.println("Error writing to file '"+ fileName + "'");
+		     // ex.printStackTrace();
+			}
+		}// end WriteFile
 	
+    public static class GUI extends JFrame {
+        
+        JTextField textField,textField_1,textField_2; //The values that are used to manipulate the data of the array
+        JLabel entry1, entry2, entry3; //References to aid the user in the program
+        private static JRadioButton checking, saving; //JRadio buttons
+        
+        private static JFrame frame; //Creates the Frame for display messages
+   
+        public GUI() {
+            
+            // Making layout GridBagLayout ()     
+            setLayout(new GridBagLayout());
+            frame = new JFrame();
+            
+            //Instance of the GUI
+            GridBagConstraints window = new GridBagConstraints();
+            
+            window.insets = new Insets (5, 5, 5, 5);
+            
+            //Incorporate the Radio Buttons and bounds
+			checking = new JRadioButton("Checking");  //Setting the Jradio button iterative as final
+			window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 0;
+            window.gridy = 3;
+            window.gridwidth = 1;
+			checking.setSelected(true); //Having the JRadio button of "iterative" selected upon opening the application
+			add(checking, window);
+			
+			saving = new JRadioButton("Savings"); //Setting the JRadio button recursive
+			window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 1;
+            window.gridy = 3;
+            window.gridwidth = 1;
+			add(saving, window);
+			
+			// Group the radio buttons to toggle between two groups
+			ButtonGroup group = new ButtonGroup();
+			group.add(checking);
+			group.add(saving);
+
+            //JButton
+			JButton withdraw = new JButton("Withdraw");
+            window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 0;
+            window.gridy = 0;
+            window.gridwidth = 1;
+            add (withdraw, window);
+            
+            JButton deposit = new JButton("Deposit");
+            window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 1;
+            window.gridy = 0;
+            window.gridwidth = 1;
+            add (deposit, window);
+            
+            JButton transfer = new JButton("Transfer to");
+            window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 0;
+            window.gridy = 1;
+            window.gridwidth = 1;
+            add (transfer, window);
+//            
+            JButton balance = new JButton("Balance");
+            window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 1;
+            window.gridy = 1;
+            window.gridwidth = 1;
+            add (balance, window);
+            
+            // Textfield 1
+            textField = new JTextField(10);
+            window.fill = GridBagConstraints.HORIZONTAL;
+            window.gridx = 0;
+            window.gridy = 4;
+            window.gridwidth = 1;
+            add (textField, window);
+            
+   
+            //Make instances of events in the program
+            Compute_Listener converter_listener = new Compute_Listener();
+            //Each event attached to listener.
+            deposit.addActionListener(converter_listener);
+            withdraw.addActionListener(converter_listener);
+            balance.addActionListener(converter_listener);
+            transfer.addActionListener(converter_listener);
+            } // end  Calculator()    
+
+      /**
+       *This class handles all the action listeners. Handles 
+       */  
+		public class Compute_Listener implements ActionListener  {
+
+					// Actions performed 
+			        @Override
+			        public void actionPerformed(ActionEvent commands){
+			            
+			            String userInput = textField.getText();
+			            String operators = commands.getActionCommand(); // Finding particular command
+			            Boolean answer = checking.isSelected(); // Getting answer from whether 
+			            
+			            if(operators.equals("Deposit")){ //
+			            	Account.despositIt(userInput, answer);   
+			            	System.out.println("Entered Deposit");       
+			            } else if (operators.equals("Withdraw")) {
+			            	Account.withdrawIt(userInput, answer);
+			            } else if (operators.equals("Balance")) {
+			            	Account.showBalance(answer); 	
+			            } else {
+			            	Account.transferTo(userInput, answer);
+			            }
+		
+			        }//End of public void actionPerformed
+			        
+		        }//End of public class Compute_Listener
+		
+    }//End of Calculator Class
+    
 	public static class Account {
 		
-	}
+		private static JFrame frame;
+		
+		//Function: checks whether or not a user's input is an integer or not
+		//Input: The users input 
+		//Output: Checks to see if the userinput can be parsed, so either an integer if it can or an error
+		static boolean checkValue(String userInput) {
+			try {
+				Integer.parseInt(userInput); // Attempts to parse the Integer
+				return true;
+			  }
+			   catch(Exception e){
+			   return false;
+			 }
+		} // end CheckValue
+		
+		//Function:
+		//Input:
+		//Output:
+		public static void showBalance(Boolean answer) {
+			if (answer) {
+				JOptionPane.showMessageDialog(frame, "Checking Balance: " + checkingAccount.toString());
+			} else {
+			JOptionPane.showMessageDialog(frame, "Savings Balance: " + savingsAccount.toString());
+			}
+		}
+		
+		//Function: 
+	    //Input: 
+	    //Output: 
+		static void despositIt(String userInput, Boolean answer) {
+			try {
+            	if (checkValue(userInput)) {
+            		int intInput = Integer.parseInt(userInput);
+            		if (answer){ //if the iterative button is selected, do the following		
+						checkingAccount += intInput; //call the computerRecursive method which will calculate fibonacci sequence iteratively
+						System.out.println(checkingAccount);
+					} else { //if the recursive button is selected do the following
+						savingsAccount += intInput;	
+						System.out.println(savingsAccount);
+						};
+					} else {
+						JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
+					}
+				} catch (Exception ex) { //If the user types in something that is not valid then do the following
+                    JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+	    } // end static deposit it
+
+		//Function: 
+	    //Input: 
+	    //Output:
+		static void withdrawIt (String userInput, Boolean answer) {
+			try {
+            	if (checkValue(userInput)) {
+            		int intInput = Integer.parseInt(userInput);
+            		if (answer){ //		
+						checkingAccount -= intInput; //
+					} else { //
+						savingsAccount -= intInput;		
+						};
+					} else {
+						JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
+					}
+				} catch (Exception ex) { //If the user types in something that is not valid then do the following
+                    JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+		}// End withdrawIt
+		
+		//Function: 
+	    //Input: 
+	    //Output:
+		static void transferTo (String userInput, Boolean answer) {
+			try {
+            	if (checkValue(userInput)) {
+            		int intInput = Integer.parseInt(userInput);
+            		if (answer){ //		
+            			checkingAccount = checkingAccount - intInput;
+						savingsAccount += intInput;
+					} else { //
+						savingsAccount = savingsAccount - intInput;
+						checkingAccount += intInput;
+						};
+					} else {
+						JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
+					}
+				} catch (Exception ex) { //
+                    JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+		}// End withdrawIt
+	} // public static class Account
 	
-	public static class InsufficientFunds {
+	public static class insufficientFunds {
 		
 	}
 	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					
-					//Build Basic JFrame
-					frame = new JFrame("ATM Machine");
-					frame.setBounds(100, 100, 450, 300);
-					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.getContentPane().setLayout(null);
-					frame.setVisible(true);
-					frame.addWindowListener(new WindowAdapter(){
-						@Override
-						public void windowClosing(WindowEvent event) {
-							confirmAndExit();
-						}
-					});
-					
-					//Incorporate the Radio Buttons and bounds
-					final JRadioButton checking = new JRadioButton("Checking");  //Setting the Jradio button iterative as final
-					checking.setSelected(true); //Having the JRadio button of "iterative" selected upon opening the application
-					checking.setBounds(235, 119, 100, 29);
-					frame.getContentPane().add(checking);
-					
-					JRadioButton savings = new JRadioButton("Savings"); //Setting the Jradio button recursive
-					savings.setBounds(100, 119, 100, 29);
-					frame.getContentPane().add(savings);
-					
-					//Group the radio buttons to toggle between two groups
-					ButtonGroup group = new ButtonGroup();
-					group.add(checking);
-					group.add(savings);
-					
-					//Create the Text Fields within the content pane
-					textField = new JTextField();  //Creating the first user input text field
-					textField.setBounds(90, 157, 250, 26);
-					frame.getContentPane().add(textField);
-					textField.setColumns(10);
-					
-					//Deposit Button
-					JButton btnCompute = new JButton("Deposit");
-					btnCompute.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							try {
-								String userInput = textField.getText();
-								int intInput = Integer.parseInt(userInput);
+	//Main Block
+    public static void main(String[] args) {
+    	GUI converter = new GUI(); //Create instance of converter 
 
-								if (checking.isSelected()){ //if the iterative button is selected, do the following
-									
-									checkingAccount += (intInput);
-									
-								} else { //if the recursive button is selected do the following
-									
-									savingsAccount += (intInput);
-								}
-
-							} catch (Exception ex) { //If the user types in something that is not valid then do the following
-			                    JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
-			                }
-
-						}
-					});
-					btnCompute.setBackground(SystemColor.textHighlight);
-					btnCompute.setBounds(238, 30, 100, 30);
-					frame.getContentPane().add(btnCompute);
-					
-					//Balance Button
-					JButton btnCompute2 = new JButton("Balance");
-					btnCompute2.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							try {
-								if (checking.isSelected()){ //if the iterative button is selected, do the following
-									
-									showBalance("Checking", checkingAccount);
-//									textField.setText(checkingAccount.toString());
-									System.out.println(checkingAccount);
-						
-								} else { //if the recursive button is selected do the following
-									
-									showBalance("Savings", savingsAccount);
-//									textField.setText(savingsAccount.toString());
-									System.out.println(savingsAccount);
-								}
-
-							} catch (Exception ex) { //If the user types in something that is not valid then do the following
-			                    JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
-			                }
-
-						}
-					});
-					btnCompute2.setBackground(SystemColor.textHighlight);
-					btnCompute2.setBounds(238, 73, 100, 30);
-					frame.getContentPane().add(btnCompute2);
-					
-					//Withdraw Button
-					JButton btnCompute3 = new JButton("Withdraw");
-					btnCompute3.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							try {
-								String userInput = textField.getText();
-								int intInput = Integer.parseInt(userInput);
-
-								if (checking.isSelected()){ //if the checking button is selected, do the following
-									
-									checkingAccount = checkingAccount - intInput;
-									
-								} else { //
-									
-									savingsAccount = savingsAccount - intInput;
-								}
-
-							} catch (Exception ex) { //If the user types in something that is not valid then do the following
-			                    JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
-			                }
-
-						}
-					});
-					btnCompute3.setBackground(SystemColor.textHighlight);
-					btnCompute3.setBounds(90, 30, 100, 30);
-					frame.getContentPane().add(btnCompute3);
-					
-					
-					//Withdraw Button
-					JButton btnCompute4 = new JButton("Transfer to");
-					btnCompute4.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							try {
-								String userInput = textField.getText();
-								int intInput = Integer.parseInt(userInput);
-
-								if (checking.isSelected()){ //if the iterative button is selected, do the following
-									
-									checkingAccount = checkingAccount - intInput;
-									savingsAccount += intInput;
-									
-								} else { //if the recursive button is selected do the following
-									
-									savingsAccount = savingsAccount - intInput;
-									savingsAccount = 0;
-								}
-
-							} catch (Exception ex) { //If the user types in something that is not valid then do the following
-			                    JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
-			                }
-
-						}
-					});
-					btnCompute4.setBackground(SystemColor.textHighlight);
-					btnCompute4.setBounds(90, 73, 100, 30);
-					frame.getContentPane().add(btnCompute4);
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-}
+         // Get basic parameters of a GUI 
+         converter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         converter.pack();
+         converter.setTitle("ATM Machine");
+         converter.setLocationRelativeTo(null);
+         converter.setVisible(true);
+    }//End main()
+    
+}//End CMISPRJ3RenickC
