d81a6e26bd03d8608e0671ee04ef12d757f219b8
Chase Renick <RWolf16@users.noreply.github.com>
2019-07-14 17:53:30 -0400

    0.0.3

--- project4/CMIS242PRJ4RenickC.java
+++ project4/CMIS242PRJ4RenickC.java
@@ -2,8 +2,7 @@
  * File: CMIS242PRJ4RenickC.java
  * Author: Chase Renick
  * Date: July 10, 2019
- * Purpose: This program is designed to do a calculation recursively and iteratively
- * based on what a user types into the program.
+ * Purpose: This program is designed to act as a database allowing a user to insert, delete, find, and update records.
  */
 
 //Installing Necessary Packages
@@ -17,7 +16,6 @@ import javax.swing.*;
 
 import java.io.*;
 import java.awt.event.*;
-import java.awt.SystemColor;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -25,7 +23,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 
-public class CMIS242PRJ4RenickC {
+public class InvestmentGUI2 {
 	
 	public static Map<String, Student> map = new HashMap<String, Student>();
 	
@@ -139,119 +137,82 @@ public class CMIS242PRJ4RenickC {
 			            String userMajor = major.getText();
 			            String selected = box.getSelectedItem().toString();
 			            String operators = commands.getActionCommand(); // Finding particular command
-			     
+			            String[] gradeOptions = {"A","B","C","D"};
+			            String[] numberOfCredits = {"1","2","4","6"};
 			            
-			            if(operators.equals("Process Request")) { // Process Request
-			            	if (selected == "Insert") {
-			            		
-			            	} else if (selected == "Insert") {
-			            		map.(userId, Student(userName,userMajor));
-//			            		map.put(intId, Student(userName, userMajor) );
-			            	} else if (selected == "Delete") {
-			            		if (map.remove(userId) != null) {
-			            			JOptionPane.showMessageDialog(null, "Student ID"+id.getText()+"is deleted.", "Message", JOptionPane.INFORMATION_MESSAGE);
-			            		} else {
-			            			JOptionPane.showMessageDialog(null, "Student ID"+id.getText()+"cannot be found in the database", "Message", JOptionPane.INFORMATION_MESSAGE);
-			            		}
-			            	} else if (selected == "Find") {
-			            		String studentId = map.get(userId);
-			            		if (student != null) {
-			            			userName.setText(student.getName());
-			            			userMajor.setText(String.copyValueOf(student.getMajor()));
-			            			JOptionPane.showMessageDialog(null, student.toString(), "Message", JOptionPane.INFORMATION_MESSAGE);
-			            		} else {
-			            			JOptionPane.showMessageDialog(null, "Could not find Student ID" +id.getText()+ " within databse.", "Message", JOptionPane.INFORMATION_MESSAGE);
-			            		}
-			            	} else if (selected == "Update") {
-			            		System.out.println("Update");
-			            	} else {
-			            		System.out.println("Unrecognized");
-			            	}
-			          
-			            }//End else
-			            
+			            if(operators.equals("Process Request")) { 
+				            //Execution if user clicks Process Request Button
+				            try {
+				            	if (selected == "Insert") { // Looks to see in combo box if insertion was highlighted
+				            		System.out.println("Inserting");
+				            		Student newStudent = new Student(userName, userMajor);
+				            		map.put(userId, newStudent);
+				            		JOptionPane.showMessageDialog(null, "Student with ID " +userId+" has been inserted into the database. ", "Message", JOptionPane.INFORMATION_MESSAGE);
+				            	} else if (selected == "Delete") { // Looks to see in combo box if deletion was highlighted
+				            		System.out.println("Deleting");
+				            		if (map.remove(userId) != null) {
+				            			JOptionPane.showMessageDialog(null, "Student with ID "+id.getText()+"has been deleted from the database.", "Message", JOptionPane.INFORMATION_MESSAGE);
+				            		} else {
+				            			JOptionPane.showMessageDialog(null, "Student with ID "+id.getText()+"cannot be found in the database", "Message", JOptionPane.INFORMATION_MESSAGE);
+				            		}
+				            	} else if (selected == "Find") { // Looks to see in combo box if find was highlighted
+				            		System.out.println("Searching");
+				            		Student existingStudent = map.get(userId);
+				            		if (existingStudent != null) {
+				            			name.setText(existingStudent.getName()); //sets the name of the student in jtextbook and searches for existing student in hashmap
+				            			major.setText(String.valueOf(existingStudent.getMajor()));
+				            			JOptionPane.showMessageDialog(null, existingStudent.summary() +" found. See text box for details", "Message", JOptionPane.INFORMATION_MESSAGE);
+				            		} else {
+				            			JOptionPane.showMessageDialog(null, "Cannot not find the student with ID" +id.getText()+ " within the database.", "Message", JOptionPane.INFORMATION_MESSAGE);
+				            		}
+				            	} else if (selected == "Update") { // Looks to see in combo box if update was highlighted
+				            		System.out.println("Updating");
+				            		
+				            		//Takes the user's selection for the update
+				            		String gradeString = (String) JOptionPane.showInputDialog(null,"Select Grade: ","",JOptionPane.QUESTION_MESSAGE,null,gradeOptions,gradeOptions[0]);
+				            		
+				            		//Calculates an integer version of the string
+				            		Integer gradeNumber = getGradeNumber(gradeString);
+				            		
+				            		String creds = (String) JOptionPane.showInputDialog(null,"Select Credit #: ","",JOptionPane.QUESTION_MESSAGE,null,numberOfCredits,numberOfCredits[0]);
+				            		Integer numofCreds = Integer.parseInt(creds);
+				            		if (map.containsKey(userId)) { //Looks to see if the userId is found in the hashmap
+				            			Student newStudent = map.get(userId); //gets out the object from the hashmap containing student data
+				            			newStudent.supplementalClass(gradeNumber, numofCreds); //adds grade and credits to the object
+				            			map.put(userId, newStudent); //puts in the upadted versio back into the hashmap
+				            			JOptionPane.showMessageDialog(null, newStudent.summary(), "Message", JOptionPane.INFORMATION_MESSAGE); //relays that information to user
+				            		}
+				            	} else {
+				            		System.out.println("Unrecognized");
+				            	}//End else
+				            } catch (Exception ex) { //If the user types in something that is not valid then do the following
+			                    JOptionPane.showMessageDialog(null, "Invalid input, please enter correct input data", "Error", JOptionPane.ERROR_MESSAGE);
+			                }
+			            }
 			        }// End actionPerformed
 
-		
-						
-			        
 		} // End Compute Listener
+		
+		
+		//Function: Returns a grade number based on the letter grade a student receives
+		//Input: A letter grade number
+		//Output: An integer representing the letter grade
+		public static Integer getGradeNumber(String gradeString) {
+			if (gradeString.equals("A")) {
+				return 4;
+			} else if (gradeString.equals("B")){
+				return 3;
+			} else if (gradeString.equals("C")) {
+				return 2;
+			} else if (gradeString.equals("C")) {
+				return 1;	
+			} else {
+				return 0;
+			}
+		}
 		
 	} // End GUI()
 	
-	public static class Study {
-		
-		private double grade;
-		private int credit;
-		
-		public Study(double grade, int credit) {
-			setGrade(grade);
-			setCredit(credit);
-		}
-		
-		public double getGrade() {
-			return grade;
-		}
-		
-		public void setGrade(double grade) {
-			this.grade = grade;
-		}
-		
-		public int getCredit() {
-			return credit;
-		}
-		
-		public void setCredit(int credit) {
-			this.credit = credit;
-		}
-	} // end Study
-	
-	public static class Student {
-		
-		private String name;
-		private String major;
-		private List<Study> list;
-		
-		public Student (String name, String major) {
-			this.name = name;
-			this.major = major;	
-		}
-		
-		public String getName() {
-			return name;
-		} //end getName
-		
-		public void setName() {
-			this.name = name;
-		} //end getMajor
-		
-		public String getMajor() {
-			return major;
-		}// end getMajor
-		
-		public void setMajor(String major) {
-			this.major = major;
-		} // end getMajor
-		
-		public Double calculateGPA(Double courseGrade, Double creditHours) {
-			int totalCredits =0;
-			double sum = 0;
-			for (Study course: list) {
-				totalCredits += course.getCredit();
-				sum += course.getGrade() * course.getCredit();
-			}
-			if (totalCredits == 0) {
-				return 0.0;
-			} else {
-				return (sum / totalCredits);
-			}
-		} // end calculateGPA
-		
-		public void increaseStudy(Double courseGrade, int creditHours) {
-			list.add(new Study(courseGrade, creditHours));
-		}// end increaseStudy
-		
-	}// End Student Class
 	
 	public static void main(String[] args) {
 		GUI database = new GUI(); //Create instance of converter 
