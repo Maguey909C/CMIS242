797e23b4d345a6b6a6ff85299b940bf370c3e270
Chase Renick <RWolf16@users.noreply.github.com>
2019-07-15 09:39:08 -0400

    0.0.2

--- project4/Student.java
+++ project4/Student.java
@@ -1,33 +1,58 @@
+/*
+ * File: CMIS242PRJ4RenickC.java
+ * Author: Chase Renick
+ * Date: July 14, 2019
+ * Purpose: This program is used to build out the type of student.
+ */
 
 import java.util.List;
+import java.util.ArrayList;
+
+//Class Student
 public class Student {
 	
 	private String name;
 	private String major;
-	private List<Study> list;
+	private List<Study> list = new ArrayList<Study>();
 	
+	//Constructor for Student name and major
 	public Student (String name, String major) {
 		this.name = name;
 		this.major = major;	
 	}
 	
+	//retrieving name from object
 	public String getName() {
 		return name;
 	} //end getName
 	
+	//setting name for the object
 	public void setName() {
 		this.name = name;
 	} //end getMajor
 	
+	//getting major
 	public String getMajor() {
 		return major;
 	}// end getMajor
 	
+	//setting major
 	public void setMajor(String major) {
 		this.major = major;
 	} // end getMajor
 	
-	public Double calculateGPA() {
+	//Function: Takes the course grade and credit hours, creates a study object and puts it in a list
+	//Input: Course grade integer and credit hours integer
+	//Output: Nothing, but it will add the object to a list
+	public void addClass(Integer courseGrade, Integer creditHours) {
+		Study n = new Study(courseGrade, creditHours);
+		list.add(n);
+	}
+	
+	//Function: Designed to calculate the total GPA based on the course credit and grade
+		//Input: Nothing
+		//Output: A string representing a student's GPA
+	public String calculateGPA() {
 		int totalCredits =0;
 		double sum = 0;
 		for (Study course: list) {
@@ -35,21 +60,23 @@ public class Student {
 			sum += course.getGrade() * course.getCredit();
 		}
 		if (totalCredits == 0) {
-			return 0.0;
+			return "0";
 		} else {
-			return (sum / totalCredits);
+			double value = (sum / totalCredits);
+			System.out.println(value);
+			return Double.toString(value);
 		}
 	} // end calculateGPA
 	
-	public void supplementalClass(Integer courseGrade, Integer creditHours) {
-		list.add(new Study(courseGrade, creditHours));
-	}
-	
+	//Function: To summarize the results from the query or update
+	//Input: nothing
+	//Output: An summary of the major information related to user
 	public String summary() {
 		StringBuilder data = new StringBuilder();
 		data.append("Name of student: " + name+"\n");
 		data.append("Student Major: " + major+"\n");
-		data.append("Student GPA: " +"1"+ "\n");
+		String response = calculateGPA();
+		data.append("Student GPA: " +response+ "\n");
 		return data.toString();
 	}
 }// End Student Class
